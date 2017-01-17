6cd101d43ae47ac076cb924a08612c9147aacc0f
Alen David Jeđud <alen.jedud@hotmail.com>
2017-01-17 14:16:47 +0100

    Postavljeno parsiranje JSON-a

--- TestCommunicationMeR/Program.cs
+++ TestCommunicationMeR/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestCommunicationMeR
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
         }
+
+        static void GetJson()
+        {
+            //string DomainControllerMethod = "https://www.moj-eracun.hr/exchange/getstatus?";
+            string url = "https://www.moj-eracun.hr/exchange/getstatus?id=1041189&ver=cb590fe1-3138-4287-b8fc-058a56065152";
+
+            //string url2 = DomainControllerMethod + ;
+            using (var MeR = new WebClient())
+            {
+                var json = MeR.DownloadString(url);
+                Console.WriteLine(json);
+                Console.ReadLine();
+                JsonResponse Result = JsonConvert.DeserializeObject<JsonResponse>(json);
+                using (StreamWriter ResultFile = File.CreateText(@"C:\Temp\TestMerJSON.txt"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(ResultFile, json);
+                }
+                int DocumentStatus = Result.Status;
+                Console.WriteLine(DocumentStatus);
+                Console.ReadLine();
+            }
+        }
+        public class JsonResponse
+        {
+            [JsonProperty]
+            public int Id { get; set; }
+            public string InterniBroj { get; set; }
+            public string SupplierName { get; set; }
+            public string SupplierID { get; set; }
+            public int Status { get; set; }
+            public int Type { get; set; }
+            public string ParentDocumentID { get; set; }
+            public DateTime IssueDate { get; set; }
+            public DateTime UpdateDate { get; set; }
+            public string Message { get; set; }
+        }
+
     }
 }
